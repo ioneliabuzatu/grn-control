46d6ba7bf775de986271855190562c59e6e16f62
SimonDufLab <Sduf.lab@gmail.com>
2022-01-28 13:04:18 -0500

    Vmapping of initialization and half-response; left to do: repeat scheme in integration loop

--- sim.py
+++ sim.py
@@ -18,7 +18,7 @@ class Sim:
         self.num_cells_to_simulate = num_cells_to_simulate
         self.adjacency = np.zeros(shape=(self.num_genes, self.num_genes))
         self.decay_lambda = 0.8
-        self.mean_expression = -1 * np.ones((num_genes, num_cells_types))
+        self.mean_expression = -1 * jnp.ones((num_genes, num_cells_types))
         self.sampling_state = 10
         self.simulation_time_steps = self.sampling_state * self.num_cells_to_simulate
         self.x = np.zeros(shape=(self.simulation_time_steps, num_genes, num_cells_types))
@@ -28,9 +28,30 @@ class Sim:
     def run(self):
         self.adjacency, graph = self.load_grn()
         layers = self.topo_sort_graph_layers(graph)
-        # print(layers)
-        basal_production_rate = self.get_basal_production_rate()
-        self.simulate_expression_layer_wise(layers, basal_production_rate)
+        print(layers)
+        basal_production_rate = self.get_basal_production_rate()  # TODO: Move to initialization?
+        # Note: basal rate is zero for non-master regulator genes
+        print(basal_production_rate.shape)
+        # print(basal_production_rate[:, 0])
+        print("above")
+        print(self.adjacency.shape)
+        # print(self.adjacency[67])
+
+        # get half_response for all genes ahead of time, for all cell
+        self.half_response = jax.vmap(jax.vmap(self.get_half_response, in_axes=(0, None)), in_axes=(None, 0))(
+            self.adjacency, self.mean_expression.T).T  # TODO: compare to previous half_response recovery function
+        print(self.half_response.shape)
+
+        # Initialize concentration of master regulators across all cells
+        init_concentration_dict = jax.vmap(self.init_master_reg_one_cell, in_axes=(1, None))(basal_production_rate, layers[0])
+        print(init_concentration_dict)
+        # Then, initialize concentration of regulated genes layer per layer, across all genes
+        init_concentration_dict = jax.vmap(self.init_regulated_one_cell, in_axes=(None, 1, 0, None))(
+            self.adjacency, self.half_response, init_concentration_dict, layers)
+        print(init_concentration_dict.keys())
+
+
+        # self.simulate_expression_layer_wise(layers, basal_production_rate)
 
     def load_grn(self):
         topo_sort_graph = nx.DiGraph()
@@ -47,7 +68,7 @@ class Sim:
                 topo_sort_graph.add_weighted_edges_from(
                     zip(regulators_nodes_ids, repeat(target_node_id), contributions)
                 )
-        return self.adjacency, topo_sort_graph
+        return jnp.array(self.adjacency), topo_sort_graph
 
     @staticmethod
     def topo_sort_graph_layers(graph: nx.DiGraph):
@@ -65,7 +86,7 @@ class Sim:
                 b_for_cell_type = np.array(list(map(float, row)))
                 basal_production_rates[master_regulator_node_id] = b_for_cell_type
 
-        return basal_production_rates
+        return jnp.array(basal_production_rates)
 
     def simulate_expression_layer_wise(self, layers, basal_production_rate):
         layers_copy = deepcopy(layers)
@@ -96,44 +117,89 @@ class Sim:
             # TODO em
             # TODO convergence
 
-    def calculate_half_response(self, layer):
-        for gene in layer:
-            regulators = np.where(self.adjacency[:, gene] != 0)
-            if regulators:  # TODO this is wrong, how to check for empty np.where?
-                mean_expression_per_cells_regulators_wise = self.mean_expression[regulators]
-                half_response = np.mean(mean_expression_per_cells_regulators_wise)
-                self.half_response[gene] = half_response
+    # def calculate_half_response(self, layer):
+    #     for gene in layer:
+    #         regulators = np.where(self.adjacency[:, gene] != 0)
+    #         if regulators:  # TODO this is wrong, how to check for empty np.where?
+    #             mean_expression_per_cells_regulators_wise = self.mean_expression[regulators]
+    #             half_response = np.mean(mean_expression_per_cells_regulators_wise)
+    #             self.half_response[gene] = half_response
 
-    def init_concentration(self, layer: list, basal_production_rate):  # TODO missing basal_production_rate
-        """
-        Initializes the concentration of all genes in the input level
-        Note: calculate_half_response_ should be run before this method
-        """
+    def get_half_response(self, gene_adjacency, cell_mean_expression):
+        regulators = (gene_adjacency != 0).astype(int)
+        return jnp.mean(regulators*cell_mean_expression)
 
-        x0 = np.zeros(shape=(self.num_genes, self.num_cell_types))
+    # def init_concentration(self, layer: list, basal_production_rate):  # TODO missing basal_production_rate
+    #     """
+    #     Initializes the concentration of all genes in the input level
+    #     Note: calculate_half_response_ should be run before this method
+    #     """
+    #
+    #     x0 = np.zeros(shape=(self.num_genes, self.num_cell_types))
+    #
+    #     for gene in layer:
+    #         rate = 0
+    #         regulators = np.where(self.adjacency[:, gene] != 0)
+    #         mean_expression = self.mean_expression[regulators]
+    #         absolute_k = np.abs(self.adjacency[regulators][:, gene])
+    #         is_repressive = np.expand_dims(self.adjacency[regulators][:, gene] < 0, -1).repeat(self.num_cell_types,
+    #                                                                                            axis=-1)
+    #         half_response = self.half_response[gene]
+    #         hill_function = self.hill_function(mean_expression, half_response, is_repressive)
+    #         rate += np.einsum("r,rt->t", absolute_k, hill_function)
+    #
+    #         x0[gene] = rate / self.decay_lambda
+    #
+    #     self.x[0, layer] = x0[layer]
 
-        for gene in layer:
-            rate = 0
-            regulators = np.where(self.adjacency[:, gene] != 0)
-            mean_expression = self.mean_expression[regulators]
-            absolute_k = np.abs(self.adjacency[regulators][:, gene])
-            is_repressive = np.expand_dims(self.adjacency[regulators][:, gene] < 0, -1).repeat(self.num_cell_types,
-                                                                                               axis=-1)
-            half_response = self.half_response[gene]
-            hill_function = self.hill_function(mean_expression, half_response, is_repressive)
-            rate += np.einsum("r,rt->t", absolute_k, hill_function)
+    def init_master_reg_one_cell(self, cell_basal_rates, first_layer):
+        layer_init_concentration = jax.vmap(self.init_master_regulator)(cell_basal_rates.take(jnp.array(first_layer)))
+        return dict(zip(first_layer, layer_init_concentration))
 
-            x0[gene] = rate / self.decay_lambda
+    def init_master_regulator(self, gene_basal_rate):
+        return gene_basal_rate/self.decay_lambda
 
-        self.x[0, layer] = x0[layer]
+    def init_regulated(self, gene_adjacency, half_response, concentration_dict):
+        return self.hill_functions_sum(gene_adjacency, half_response, concentration_dict)/self.decay_lambda
 
-    def hill_function(self, regulators_concentration, half_response, is_repressive):
-        rate = np.power(regulators_concentration, self.hill_coefficient) / (
-                    np.power(half_response, self.hill_coefficient) + np.power(regulators_concentration,
-                                                                              self.hill_coefficient))
+    def init_regulated_one_cell(self, adjacency, half_response, init_concentration_dict, layers):
+        for layer in layers[1:]:
+            layer_init_concentration = jax.vmap(self.init_regulated, in_axes=(1, None, None))(
+                adjacency.take(jnp.array(layer), axis=1), half_response, init_concentration_dict
+            )
+            init_concentration_dict.update(dict(zip(layer, layer_init_concentration)))
+        return init_concentration_dict
 
-        rate[is_repressive] = 1 - rate[is_repressive]
-        return rate
+    # def hill_function(self, regulators_concentration, half_response, is_repressive):
+    #     rate = np.power(regulators_concentration, self.hill_coefficient) / (
+    #                 np.power(half_response, self.hill_coefficient) + np.power(regulators_concentration,
+    #                                                                           self.hill_coefficient))
+    #
+    #     rate[is_repressive] = 1 - rate[is_repressive]
+    #     return rate
+
+    def hill_functions_sum(self, gene_adjacency, half_response, concentration_dict):
+        # # Damn, can't filter out zero entries because of jax jit/vmap size compliance requirements
+        # nonzero_indices = jnp.nonzero(gene_adjacency)
+        # reduced_gene_adjacency = jnp.take(gene_adjacency, nonzero_indices)
+        # reduced_half_response = jnp.take(half_response, nonzero_indices) # Expected to be gene dependent
+        # reduced_regulators_concentration = jnp.array([concentration_dict[i] for i in nonzero_indices])
+
+        # Let's take regulators indices then
+        kept_indices = [int(k) for k in concentration_dict]
+        reduced_gene_adjacency = jnp.take(gene_adjacency, jnp.array(kept_indices))
+        reduced_half_response = jnp.take(half_response, jnp.array(kept_indices)) # Expected to be gene dependent
+        reduced_regulators_concentration = jnp.array([concentration_dict[i] for i in kept_indices])
+
+        common = jnp.power(reduced_regulators_concentration, self.hill_coefficient)
+        # print(reduced_half_response.shape)
+        # raise SystemExit(0)
+        rate = common / (jnp.power(reduced_half_response, self.hill_coefficient) + common)
+
+        activator_hill = reduced_gene_adjacency*rate
+        repressor_hill = reduced_gene_adjacency * (1-rate)
+
+        return jnp.sum(jnp.where(reduced_gene_adjacency >= 0, activator_hill, repressor_hill))
 
     def calculate_production_rate(self):
         return
