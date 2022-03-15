ffc7a14e5451aebffbfc806dca0e765cbe5b6bb5
manuel <manuel.delverme@gmail.com>
2022-03-15 16:56:47 -0400

    First version.

--- gym_gene_control/envs/grn_control_simple.py
+++ gym_gene_control/envs/grn_control_simple.py
@@ -1,8 +1,6 @@
 import gym
-import jax
 import jax.numpy as jnp
 import numpy as np
-from gym import spaces
 
 import jax_simulator
 import src.models.expert.classfier_cell_state
@@ -15,45 +13,45 @@ class GRNControlSimpleEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        params = {'num_genes': 100, 'NUM_SIM_CELLS': 100}
+        self.noise_amplitude = 0.1
+        self.num_cells_to_sim = 3
 
         dataset_dict = src.zoo_functions.open_datasets_json(return_specific_dataset='Dummy')
         dataset = src.zoo_functions.dataset_namedtuple(*dataset_dict.values())
         self.sim = jax_simulator.Sim(
-            num_genes=params["num_genes"],
-            num_cells_types=params["NUM_SIM_CELLS"],
-            simulation_num_steps=params['NUM_SIM_CELLS'],
+            num_genes=dataset.tot_genes,
+            num_cells_types=dataset.tot_cell_types,
+            simulation_num_steps=self.num_cells_to_sim,
             interactions_filepath=dataset.interactions,
             regulators_filepath=dataset.regulators,
-            noise_amplitude=0.5
+            noise_amplitude=self.noise_amplitude,
         )
         self.sim.build()
-        self.action_space = spaces.Box(low=np.zeros(self.sim.num_genes), high=np.ones(self.sim.num_genes))
-        self.observation_space = spaces.Box(low=np.zeros(self.sim.num_genes), high=np.ones(self.sim.num_genes))
+        self.action_space = gym.spaces.Box(low=np.zeros(self.sim.num_genes), high=np.ones(self.sim.num_genes))
+        self.observation_space = gym.spaces.Box(low=np.zeros(self.sim.num_genes), high=np.ones(self.sim.num_genes))
         self.initial_state = None
 
     def step(self, action):
-        final_gene_expression_dict = self.sim.run_one_rollout(action)
-        final_gene_expression = self.to_numpy(final_gene_expression_dict)
+        x_T = self.sim.run_one_rollout(action)
+        x_T = self.to_array(x_T)
 
-        target_gene_expression = self.initial_state[:, :, self.target_gene_type]
-        dist = np.linalg.norm(final_gene_expression - target_gene_expression[self.target_gene_type])
+        desired_concentration = self.initial_state[:, :, self.target_gene_type].mean(axis=0)  # TODO: worry about outliers, zeros and negatives
+        dist = jnp.linalg.norm(np.tile(desired_concentration.reshape(1, -1, 1), (1, 1, x_T.shape[2])) - x_T)
 
         reward = -dist
         done = False
 
         # TODO: return bad reward if there is no convergence
-        return final_gene_expression, reward, done, {}
+        return x_T, reward, done, {}
 
-    def to_numpy(self, final_gene_expression):
-        final_gene_expression = jnp.stack(tuple([final_gene_expression[gene] for gene in range(self.sim.num_genes)])).swapaxes(0, 1)
-        final_gene_expression = jnp.concatenate(final_gene_expression, axis=1).T
-        return final_gene_expression
+    def to_array(self, x):
+        expr_clean = jnp.stack(tuple([x[gene] for gene in range(self.sim.num_genes)])).swapaxes(0, 1)
+        return expr_clean
 
     def reset(self):
         if self.initial_state is None:
             action = jnp.zeros(self.sim.num_genes)
-            self.initial_state = self.to_numpy(self.sim.run_one_rollout(action))
+            self.initial_state = self.to_array(self.sim.run_one_rollout(action))
         return self.initial_state
 
     def render(self, mode='human'):
