4d31aea26e60f19edde6d3dbd33e56755b918a08
ioneliabuzatu <buzatuionelia@gmail.com>
2022-08-01 04:44:10 -0400

    bandits#train_a2c##steer:0#runRL:0

--- model_free_grn_control.py
+++ model_free_grn_control.py
@@ -14,7 +14,9 @@ def main():
     if whoami == 'ionelia':
         filepath='data/rl_agents_params.json'
     elif whoami == 'ionelia.buzatu':
-        filepath = "todo/rl_agents_params.json"
+        filepath = "/network/projects/_groups/grn_control/graphD0D18genes#18/thesis_rl_agents_params.json"
+
+    print(f"Loading JSON file from: {filepath}")
     json_params = open_datasets_json(filepath=filepath)
 
     agent_to_train = [train_a2c, train_td3, train_sac, train_ppo, train_ddpg]
@@ -32,12 +34,12 @@ def main():
         run_prefix = f"bandits#{agent.__name__}"
         run_suffix = f"#steer:{generals_params['target_cell_type']}#runRL:{generals_params['run_rl']}"
         wandb_run_name = f"{run_prefix}#{run_suffix}"
-        print(f"run {wandb_run_name} in progress...")
+        print(f"run ***{wandb_run_name}*** in progress...")
 
         buddy = experiment_buddy.deploy(
             host=generals_params['host'], disabled=False,
             wandb_kwargs={
-                'sync_tensorboard': False,
+                'sync_tensorboard': True,
                 'monitor_gym': True,
                 'save_code': True,
                 'entity': 'control-grn',
